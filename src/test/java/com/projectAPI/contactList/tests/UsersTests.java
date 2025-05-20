fc886f23b23602616e31c597b33f4d0a727d8421
dianaBn <diana.balanuta.work@gmail.com>
2025-05-20 11:18:37 -0400

    Finalize user-related test cases and token management

--- src/test/java/com/projectAPI/contactList/tests/UsersTests.java
+++ src/test/java/com/projectAPI/contactList/tests/UsersTests.java
@@ -6,25 +6,14 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.*;
 
-import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UsersTests extends ContactListBasePage{
 
-    public static String token;
-    static String email = "canel@practice.com";
-    static String password = "lora1234";
-
-    @BeforeAll
-    public static void setupToken() {
-        token = ContactListUtils.getToken(email, password);
-        assertNotNull(token, "Token should not be null");
-        System.out.println("TOKEN FROM @BeforeAll: " + token);
-    }
-
-
+    public static String email = "canel@practice.com";
+    public static String password = "lora1234";
 
 
     /**
@@ -37,14 +26,6 @@ public class UsersTests extends ContactListBasePage{
     @Order(1)
     @Test
     public void TC01CreateNewUser(){
-        /*
-        HashMap<String, String> user = new HashMap<>();
-        user.put("firstName", "Sony");
-        user.put("lastName", "Hunnam");
-        user.put("email", "shunnam@practice.com");
-        user.put("password", "sony1234");
-
-         */
 
         String newUser = "{"
                 + "\"firstName\": \"Lora\","
@@ -59,25 +40,25 @@ public class UsersTests extends ContactListBasePage{
                 .body(newUser)
                 .when()
                 .post("/users");
-                //.then().log().all()
-                //.statusCode(201);
 
         assertEquals(201, response.statusCode(), "User should be created successfully");
         System.out.println("User registration response: " + response.asString());
 
-        // login and store token
-        //token = ContactListUtils.getToken("canel@practice.com", "lora1234");
-        //assertNotNull(token);
-        //System.out.println("Token: " + token);
+        // 2. Login to get token
+        //token = ContactListUtils.getToken(email, password);
+
 
     }
 
     @Test
     @Order(2)
     public void TC02LoginAndGetToken() {
-        token = ContactListUtils.getToken(email, password);
+
+        String token = ContactListUtils.getToken(email, password);
         assertNotNull(token);
         System.out.println("TOKEN: " + token);
+        System.out.println("email = " + email);
+
     }
 
 
@@ -87,23 +68,22 @@ public class UsersTests extends ContactListBasePage{
      * Then the response status should be 200 (OK)
      * And the response body should contain my user profile information
      */
-    @Order(2)
+    @Order(3)
     @Test
     public void TC02GetUserProfile(){
-
+        String token = ContactListUtils.getToken(email, password);
         System.out.println("Token: " + token);
 
-        //Response response =
+        Response response =
         given()
                 .header("Authorization", "Bearer " + token)
                 .when()
-                .get("/users/me")
-              .then().log().all()
-              .statusCode(200);
+                .get("/users/me");
 
-        //assertEquals(200, response.statusCode(), "Status code should be 200");
 
-        //System.out.println("User Profile: " + response.asPrettyString());
+        assertEquals(200, response.statusCode(), "Status code should be 200");
+
+        System.out.println("User Profile: " + response.asPrettyString());
 
 
     }
@@ -116,9 +96,10 @@ public class UsersTests extends ContactListBasePage{
      * Then the response status should be 200 (OK)
      * And the response body should reflect the updated user information
      */
-    @Order(3)
+    @Order(4)
     @Test
     public void updateUserProfile() {
+        String token = ContactListUtils.getToken(email, password);
 
         Response getResponse = given()
                 .contentType(ContentType.JSON)
@@ -131,10 +112,10 @@ public class UsersTests extends ContactListBasePage{
         String userId = getResponse.jsonPath().getString("_id");
         assertNotNull(userId, "User ID should not be null");
 
-        // Prepare updated details
+        // updated details
         String updatedInfo = "{ \"firstName\": \"Cely\", \"lastName\": \"Stones\" }";
 
-        // Then: Send PATCH request
+
         Response patchResponse = given()
                 .contentType(ContentType.JSON)
                 .header("Authorization", "Bearer " + token)
@@ -162,9 +143,10 @@ public class UsersTests extends ContactListBasePage{
      * Then the response status should be 200 (OK)
      * And the response body should confirm a successful logout
      */
-    @Order(4)
+    @Order(5)
     @Test
     public void TC04LogoutRequest(){
+        String token = ContactListUtils.getToken(email, password);
 
         Response response = given()
                 .header("Authorization", "Bearer " + token)
@@ -184,9 +166,10 @@ public class UsersTests extends ContactListBasePage{
      * Then the response status should be 204 (No Content)
      * And the user should be deleted with no content returned in the response
      */
-    @Order(5)
+    @Order(6)
     @Test
     public void TC05DeleteUser(){
+        String token = ContactListUtils.getToken(email, password);
 
         Response response = given()
                 .header("Authorization", "Bearer " + token)
@@ -194,6 +177,7 @@ public class UsersTests extends ContactListBasePage{
                 .delete("users/me");
 
         assertEquals(200, response.getStatusCode());
+        System.out.println("User with email:" + email + "--> was successfully deleted.");
 
 
     }
